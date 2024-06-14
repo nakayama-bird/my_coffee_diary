4b8c1033c45b4abc4d54ccfd4da7fefba4fb3886
中嶋ひかる <nakayama_bird@nakajimahikarunoMacBook-Air.local>
2024-06-14 11:27:30 +0900

    投稿編集ページの追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'staticpages#top'
 
   resources :users, only: %i[new create]
-  resources :posts, only: %i[index new create show destroy] 
+  resources :posts
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
