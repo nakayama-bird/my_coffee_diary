2b9d52c7b8f6c6582f4740a3958dd0cda2829818
中嶋ひかる <nakayama_bird@nakajimahikarunoMacBook-Air.local>
2024-06-15 14:02:19 +0900

    Xシェア機能、OGPの設定、GA4の設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,31 @@ module ApplicationHelper
         else "bg-gray-500"
         end
     end
+
+
+    def default_meta_tags
+        {
+            site: 'My Coffee Diary',
+            title: '毎日のコーヒーを記録するアプリ',
+            reverse: true,
+            charset: 'utf-8',
+            description: 'My Coffee Timeは今日楽しんだコーヒーを記録するアプリです',
+            keywords: 'コーヒー,カフェラテ,投稿',
+            canonical: request.original_url,
+            separator: '|',
+            og: {
+                site_name: :site,
+                title: :title,
+                description: :description,
+                type: 'website',
+                url: request.original_url,
+                image: image_url('ogp.png'),
+                local: 'ja-JP'
+            },
+            twitter: {
+                card: 'summary_large_image', 
+                image: image_url('ogp.png')
+            }
+        }
+    end
 end
